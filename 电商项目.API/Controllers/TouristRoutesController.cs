a0f2ffebd393cbd35ad19ef95ff1bdea54bcf572
spf <1771245717@qq.com>
2022-08-27 00:00:16 +0800

    修改为 http启动

---
+++ 电商项目.API/Controllers/TouristRoutesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using 电商项目.API.Services;
+
+namespace 电商项目.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TouristRoutesController : ControllerBase
+    {
+        private readonly ITouristRouteRespository _touristRouteRespository;
+
+        public TouristRoutesController(ITouristRouteRespository touristRouteRespository)
+        {
+            _touristRouteRespository = touristRouteRespository 
+                ?? throw new NotImplementedException(nameof(ITouristRouteRespository));
+        }
+
+        [HttpGet]
+        [Route("GetTouristRoutes")]
+        public IActionResult GetTouristRoutes()
+        {
+            var routs = _touristRouteRespository.GetTouristRoutes();
+
+            return this.Ok(routs);
+        }
+    }
+}
--- 电商项目.API/Program.cs
+++ 电商项目.API/Program.cs
@@ -6,17 +6,13 @@ internal class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
-
         _ = builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         _ = builder.Services.AddEndpointsApiExplorer();
         _ = builder.Services.AddSwaggerGen();
         _ = builder.Services.AddTransient<ITouristRouteRespository, MockTouristRouteRespository>();
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
             _ = app.UseSwagger();
