bdfd14065b36c236b6393ab73e1055adbf5e0397
spf <1771245717@qq.com>
2022-08-26 22:51:11 +0800

    模型添加

---
+++ 电商项目.API/Moldes/TouristRoute.cs
@@ -0,0 +1,32 @@
+﻿namespace 电商项目.API
+{
+    /// <summary>
+    /// 旅游路线
+    /// </summary>
+    public class TouristRoute
+    {
+        public Guid Id { get; set; }
+
+        public string? Title { get; set; }
+
+        public string? Description { get; set; }
+        //原价
+        public decimal OriginaPrice { get; set; }
+        //折扣
+        public double DiscountPresent { get; set; }
+
+        public DateTime CreateTime { get; set; }
+
+        public DateTime UpdateTime { get; set; }
+        //出发时间
+        public DateTime DepartureTime { get; set; }
+        //卖点介绍
+        public string? Feature { get; set; }
+        //费用说明
+        public string? Fees { get; set; }
+
+        public string? Notes { get; set; }
+
+        public ICollection<TouristRoutePicture>? TouristRoutePictures { get; set; }
+    }
+}
---
+++ 电商项目.API/Moldes/TouristRoutePicture.cs
@@ -0,0 +1,16 @@
+﻿namespace 电商项目.API
+{
+    /// <summary>
+    /// 旅游路线照片类
+    /// </summary>
+    public class TouristRoutePicture
+    {
+        public int Id { get; set; }
+
+        public string? Url { get; set; }
+
+        public Guid TouristRouteId { get; set; }
+
+        public TouristRoute? TouristRoute { get; set; }
+    }
+}
