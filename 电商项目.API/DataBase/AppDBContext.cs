a2926d88f429ae1c6f672b6067b3638e49b0b1e6
spf <1771245717@qq.com>
2022-08-29 20:54:27 +0800

    数据库创建

--- 电商项目.API/DataBase/AppDBContext.cs
+++ 电商项目.API/DataBase/AppDBContext.cs
@@ -5,7 +5,9 @@ namespace 电商项目.API.DataBase
 {
     public class AppDBContext : DbContext
     {
+#pragma warning disable CS8618 // 在退出构造函数时，不可为 null 的字段必须包含非 null 值。请考虑声明为可以为 null。
         public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+#pragma warning restore CS8618 // 在退出构造函数时，不可为 null 的字段必须包含非 null 值。请考虑声明为可以为 null。
         {
         }
 
